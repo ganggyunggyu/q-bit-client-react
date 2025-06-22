98b728c9669091cab1e18c96bbd517d9b80b08f7
ganggyunggyu <qwzx16@naver.com>
2025-06-22 15:21:39 +0900

    fix: 상태에 따른 리렌더링 방지

--- src/app/store/use-ui-store/index.ts
+++ src/app/store/use-ui-store/index.ts
@@ -1,12 +1,15 @@
 import { create } from 'zustand';
+import { subscribeWithSelector } from 'zustand/middleware';
 
 type UiStore = {
   isCalendarBottomSheetOpen: boolean;
   setIsCalendarBottomSheetOpen: (isOpen: boolean) => void;
 };
 
-export const useUiStore = create<UiStore>((set) => ({
-  isCalendarBottomSheetOpen: false,
-  setIsCalendarBottomSheetOpen: (isOpen) =>
-    set({ isCalendarBottomSheetOpen: isOpen }),
-}));
+export const useUiStore = create<UiStore>()(
+  subscribeWithSelector((set) => ({
+    isCalendarBottomSheetOpen: false,
+    setIsCalendarBottomSheetOpen: (isOpen) =>
+      set({ isCalendarBottomSheetOpen: isOpen }),
+  })),
+);
