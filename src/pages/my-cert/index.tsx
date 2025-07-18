cfbf49d81aa34ef859f00fa93a52f72ebf1c45c7
ganggyunggyu <qwzx16@naver.com>
2025-07-18 14:43:05 +0900

    style: layout update

--- src/pages/my-cert/index.tsx
+++ src/pages/my-cert/index.tsx
@@ -3,7 +3,7 @@ import { TitleAppBar } from '@/widgets';
 import { useGetMyRemindCerts } from '@/entities/cert/hooks/cert.hooks';
 import { Cert } from '@/entities/cert/model/cert.model';
 import { Tabs, Button, useRouter } from '@/shared';
-import { CertCard } from '@/features'; // CertCard 컴포넌트 임포트 (가정)
+import { CertCard } from '@/features';
 
 export const MyCertPage = () => {
   const { navigate } = useRouter();
@@ -23,7 +23,7 @@ export const MyCertPage = () => {
   };
 
   return (
-    <main className="flex flex-col h-[calc(100vh-150px)]">
+    <main className="flex flex-col">
       <TitleAppBar title="내 자격증" />
 
       <Tabs
@@ -36,18 +36,18 @@ export const MyCertPage = () => {
         onSelect={handleTabClick}
       />
 
-      <section className="flex-1 flex flex-col justify-center items-center bg-alternative">
+      <section className="flex-1 flex flex-col">
         {selectedTab === 'remind' &&
           (isRemindCertsLoading ? (
             <p>로딩 중...</p>
           ) : remindCerts && remindCerts.length > 0 ? (
             <div className="flex flex-col gap-4 w-full px-4 py-4">
               {remindCerts.map((cert: Cert) => (
-                <CertCard key={cert._id} cert={cert} dDay={cert.daysLeft} /> // dDay는 임시값
+                <CertCard key={cert._id} cert={cert} dDay={cert.daysLeft} />
               ))}
             </div>
           ) : (
-            <div className="flex flex-col items-center gap-4">
+            <div className="flex flex-col justify-center items-center gap-4">
               <p className="font-body ">리마인드 할 자격증이 없어요.</p>
               <p className="text-body-s text-black-tertiary">
                 어떤 자격증이 있나 살펴볼까요?
@@ -58,7 +58,7 @@ export const MyCertPage = () => {
             </div>
           ))}
         {selectedTab === 'bookmark' && (
-          <div className="flex flex-col items-center gap-4">
+          <div className="flex flex-col justify-center items-center gap-4">
             <p className="text-body-m text-black-primary">
               찜한 자격증이 없어요.
             </p>
